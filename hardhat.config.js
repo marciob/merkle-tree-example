845a67914ffd84d1067f52e4e4d56d85927ebc2d
m <13090422+marciob@users.noreply.github.com>
2023-06-17 22:05:51 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_URL}`;
+
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [MUMBAI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  },
+};
