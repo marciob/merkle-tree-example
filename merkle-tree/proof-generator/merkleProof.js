de168744eeba49f3725eaa4780eda8eeba6d75fc
m <13090422+marciob@users.noreply.github.com>
2023-06-18 13:07:16 -0300

    small improvements

--- merkle-tree/proof-generator/merkleProof.js
+++ merkle-tree/proof-generator/merkleProof.js
@@ -1,12 +1,13 @@
 const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
 const fs = require("fs");
 
-// (1)
+// it loads the tree from the file tree.json
 const tree = StandardMerkleTree.load(
-  JSON.parse(fs.readFileSync("./../tree.json"))
+  JSON.parse(fs.readFileSync("./../building-tree/tree.json"))
 );
 
-// (2)
+// it iterates over the tree to find the index of the value we want to generate the proof for
+// if the value is found, it generates the proof and prints it
 for (const [i, v] of tree.entries()) {
   if (v[0] === "0x2f0b23f53734252bda2277357e97e1517d6b042d") {
     // (3)
@@ -17,3 +18,4 @@ for (const [i, v] of tree.entries()) {
     return;
   }
 }
+console.log(false);
