46b9d3f1f1231b5e6059abb716a2d168158d9d86
m <13090422+marciob@users.noreply.github.com>
2023-06-18 13:07:33 -0300

    small improvements

--- merkle-tree/building-tree/merkle_tree_building.js
+++ merkle-tree/building-tree/merkle_tree_building.js
@@ -1,7 +1,7 @@
 const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
 const fs = require("fs");
 
-// (1)
+// define the values to be used in the tree
 const values = [
   ["0x2f0b23f53734252bda2277357e97e1517d6b042a"],
   ["0x2f0b23f53734252bda2277357e97e1517d6b042b"],
@@ -12,11 +12,12 @@ const values = [
   ["0x2f0b23f53734252bda2277357e97e1517d6b0420"],
 ];
 
-// (2)
+// create the tree
 const tree = StandardMerkleTree.of(values, ["address"]);
 
-// (3)
 console.log("Merkle Root:", tree.root);
 
-// (4)
+// saves the tree in a file called tree.json in the same folder
+// it will be used in the next script to generate the proof for a given value in the tree
+// the proof will be used to verify the value in the smart contract
 fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
